e14262cc2d4c286e9168a5619943bb2517383746
takashi <tks.srtr89@gmail.com>
2025-02-18 21:13:03 +0900

    update

--- e2e-tests/my-awesome-reporter.ts
+++ e2e-tests/my-awesome-reporter.ts
@@ -1,41 +1,58 @@
 import type {
-    Reporter,
-    FullConfig,
-    Suite,
-    TestCase,
-    TestResult,
-    FullResult,
-  } from "@playwright/test/reporter";
-  
-  class MyReporter implements Reporter {
-    constructor(options: { customOption?: string } = {}) {
-      console.log(
-        `my-awesome-reporter setup with customOption set to ${options.customOption}`
-      );
-    }
-  
-    onBegin(config: FullConfig, suite: Suite) {
-      console.log(`Starting the run with ${suite.allTests().length} tests`);
-    }
-  
-    onTestBegin(test: TestCase) {
-      console.log(`Starting test ${test.title}`);
-    }
-  
-    onTestEnd(test: TestCase, result: TestResult) {
-      console.log(`Finished test ${test.title}: ${result.status}`);
-      console.log(`Snippet : ${result?.error?.snippet}`);
-    }
-  
-    onStdErr(chunk: Buffer, test: TestCase, result: TestResult) {
-      console.log(`onStdErr chunk : ${chunk}`);
-      console.log(`onStdErr test : ${test}`);
-      console.log(`onStdErr result : ${result}`);
-    }
-  
-    onEnd(result: FullResult) {
-      console.log(`Finished the run: ${result.status}`);
-    }
+  Reporter,
+  FullConfig,
+  Suite,
+  TestCase,
+  TestResult,
+  FullResult,
+  TestError,
+} from '@playwright/test/reporter';
+
+class MyReporter implements Reporter {
+  constructor(options: { customOption?: string } = {}) {
+    console.log(
+      `[my-awesome-reporter] setup with customOption set to ${options.customOption}`
+    );
   }
-  export default MyReporter;
-  
+
+  onBegin(config: FullConfig, suite: Suite) {
+    console.log(
+      `[my-awesome-reporter] Starting the run with ${
+        suite.allTests().length
+      } tests`
+    );
+  }
+
+  onTestBegin(test: TestCase) {
+    console.log(`[my-awesome-reporter] Starting test ${test.title}`);
+  }
+
+  onTestEnd(test: TestCase, result: TestResult) {
+    console.log(
+      `[my-awesome-reporter] Finished test ${test.title}: ${result.status}`
+    );
+    console.log(`[my-awesome-reporter] Snippet : ${result?.error?.snippet}`);
+    console.log(`[my-awesome-reporter] Message : ${result?.error?.message}`);
+  }
+
+  onStdErr(chunk: Buffer, test: TestCase, result: TestResult) {
+    console.log(`[my-awesome-reporter] onStdErr chunk : ${chunk}`);
+    console.log(`[my-awesome-reporter] onStdErr test : ${test}`);
+    console.log(`[my-awesome-reporter] onStdErr result : ${result}`);
+  }
+
+  onStdOut(chunk: Buffer, test: TestCase, result: TestResult) {
+    console.log(`[my-awesome-reporter] onStdOut chunk : ${chunk}`);
+    console.log(`[my-awesome-reporter] onStdOut test : ${test}`);
+    console.log(`[my-awesome-reporter] onStdOut result : ${result}`);
+  }
+
+  onError(error: TestError) {
+    console.log(`[my-awesome-reporter] onError error : ${error}`);
+  }
+
+  onEnd(result: FullResult) {
+    console.log(`[my-awesome-reporter] Finished the run: ${result.status}`);
+  }
+}
+export default MyReporter;
